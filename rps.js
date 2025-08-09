d3ef9634a6fcc1ae10ee78d49c63b53ee14f3a1b
Huang <diamondmegalodon@gmail.com>
2025-08-09 21:21:41 +0300

    write pseudocode algorithm

--- rps.js
+++ rps.js
@@ -1,1 +1,17 @@
-console.log("hello-world")
+// define init variable for the game
+
+/// wrap these in while loop
+
+    /// get machine choice with Math.random function
+
+    /// output list of choices to user and ask user to pick one
+    /// get user input and store it in var "userInput"
+
+    /// inspect choices and peek a winner
+    /// record a winning score
+    /// output a winner
+    /// output user and machine choices
+    /// output user and machine scores
+
+/// output final rusults
+
