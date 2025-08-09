28315993db27206e8ce1221d7d3ffbee83938ee4
Huang <diamondmegalodon@gmail.com>
2025-08-09 21:36:37 +0300

    Add init functions

--- rps.js
+++ rps.js
@@ -1,17 +1,37 @@
 // define init variable for the game
+let ComputerChoice = 0,
+    ComputerScore = 0,
+    UserChoice = 0,
+    UserScore = 0,
+    RoundsPlayed = 0
 
-/// wrap these in while loop
-
-    /// get machine choice with Math.random function
-
-    /// output list of choices to user and ask user to pick one
+/// get machine choice with Math.random function
+function getComputerChoice() {
+}
+/// output list of choices to user and ask user to pick one
+function getUserChoice() {
     /// get user input and store it in var "userInput"
+}
 
-    /// inspect choices and peek a winner
-    /// record a winning score
+/// inspect choices and peek a winner
+function defineWinner() {
+    /// record a winning scor
+}
+function outputWinnerAndScore() {
     /// output a winner
     /// output user and machine choices
     /// output user and machine scores
+    /// output number of played rounds
+}
+function playRound() {
+    
+}
+
+
+/// wrap functions in while loop and play game
+while (RoundsPlayed != 5) {
+    playRound()
+}
 
 /// output final rusults
-
+outputWinnerAndScore()
